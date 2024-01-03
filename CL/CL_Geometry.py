d79de376f68874e6299227a5ddc9e076941a3f7a
raulpenaguiao <92503059+raulpenaguiao@users.noreply.github.com>
2024-01-03 10:02:11 +0100

    unit tests geometry, 285 things

--- CL/CL_Geometry.py
+++ CL/CL_Geometry.py
@@ -1,4 +1,4 @@
-from math import acos, sqrt
+from math import acos, sqrt, sin, cos, pi
 
 class Point:
     def __init__(self, x, y):
@@ -14,10 +14,20 @@ class Point:
 
 
     def Plus(self, vector):
-        pt = Point(0, 0)
+        pt = Point(self.x, self.y)
         pt.add(vector)
         return pt
 
+    def rotate(self, alpha):
+        newx = self.x * cos(alpha) - self.y * sin(alpha)
+        self.y = self.x * sin(alpha) + self.y * cos(alpha)
+        self.x = newx
+    
+
+    def Rotation(self, alpha):
+        pt = Point(self.x, self.y)
+        pt.rotate(alpha)
+        return pt
 
 class Vector:
     def __init__(self, x, y):
@@ -46,3 +56,19 @@ def TriangleArea(P1, P2, P3):
     v1 = Vector(P1.x - P2.x, P1.y - P2.y)
     v2 = Vector(P3.x - P2.x, P3.y - P2.y)
     return v1.innerProduct(v2)/2
+
+import random
+random.seed(73)
+#Test for translation and rotation
+for i in range(100):
+    A = Point(random.random(), random.random())
+    B = Point(random.random(), random.random())
+    C = Point(random.random(), random.random())
+    vec = Vector(random.random(), random.random())
+    theta = random.random()*2*pi
+    if abs(TriangleArea(A, B, C) - TriangleArea(A.Plus(vec).Rotation(theta), B.Plus(vec).Rotation(theta), C.Plus(vec).Rotation(theta))) > 10**-4:
+        print("Unit test failed for TriangleArea")
+        print(A, B, C, vec, theta)
+    if abs(AnglePoints(A, B, C) - AnglePoints(A.Plus(vec).Rotation(theta), B.Plus(vec).Rotation(theta), C.Plus(vec).Rotation(theta))) > 10**-4:
+        print("Unit test failed for AnglePoints")
+        print(A, B, C, vec, theta)
--- problems_code/Euler285/Euler285.py
+++ problems_code/Euler285/Euler285.py
@@ -4,26 +4,43 @@
 
 
 import time
-from math import sqrt
+from math import sqrt, pi
 from ...CL.CL_Rational import Rational
 from ...CL.CL_Geometry import Point, TriangleArea, AnglePoints
 start = time.time()
 ans = 0
+verbose = True
 
 def Proba(k):
-    Ok = Point(Rational(1, k).toFloat(), Rational(1, k).toFloat())
+    Ok = Point(Rational(-1, k).toFloat(), Rational(-1, k).toFloat())
     rs = Rational(1)-Rational(1, 2*k)
     rS = Rational(1)+Rational(1, 2*k)
     l = ((rs-Rational(1, k))*(rs+Rational(1, k))).toFloat()
     if l < 0:
         s = 0
     else:
-        s = Rational(1, k).toFloat() + sqrt(((rs-Rational(1, k))*(rs+Rational(1, k))).toFloat())
-    S = Rational(1, k).toFloat() + sqrt(((rS-Rational(1, k))*(rS+Rational(1, k))).toFloat())
+        s = Rational(-1, k).toFloat() + sqrt(((rs-Rational(1, k))*(rs+Rational(1, k))).toFloat())
+    S = Rational(-1, k).toFloat() + sqrt(((rS-Rational(1, k))*(rS+Rational(1, k))).toFloat())
     A = Point(s, 0)
     B = Point(S, 0)
     C = Point(0, s)
     D = Point(0, S)
+    if verbose:
+        print("----------------  k -> ", k)
+        print("Big radius = ", rS.toFloat())
+        print("Small radius = ", rs.toFloat())
+        print("O = ", Ok)
+        print("A = ", A)
+        print("B = ", B)
+        print("C = ", C)
+        print("D = ", D)
+        print("Big angle degrees = ", AnglePoints(D, Ok, B)*180/pi)
+        print("Big circle section = ", (rS.toFloat())*AnglePoints(D, Ok, B))
+        print("Small angle degrees = ", AnglePoints(C, Ok, A)*180/pi)
+        print("Small circle section = ", (rs.toFloat())*AnglePoints(C, Ok, A))
+        print("Upper triangle = ", abs(TriangleArea(Ok, A, B)))
+        print("Lower triangle = ", abs(TriangleArea(Ok, C, D)))
+        print("Area = ",  (rS.toFloat())*AnglePoints(D, Ok, B) - (rs.toFloat())*AnglePoints(C, Ok, A) - abs(TriangleArea(Ok, A, B)) - abs(TriangleArea(Ok, C, D)) )
     return (rS.toFloat())*AnglePoints(D, Ok, B) - (rs.toFloat())*AnglePoints(C, Ok, A) - abs(TriangleArea(Ok, A, B)) - abs(TriangleArea(Ok, C, D))
 LIM = 10
 
