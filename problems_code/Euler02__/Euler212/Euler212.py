ec6bb114b622be2ccb7e5a76a3d8c0d5504d6990
raulpenaguiao <92503059+raulpenaguiao@users.noreply.github.com>
2024-06-05 15:51:36 +0200

    error in 212 discovered
    Some cumes have negative volume!

--- problems_code/Euler02__/Euler212/Euler212.py
+++ problems_code/Euler02__/Euler212/Euler212.py
@@ -1,7 +1,8 @@
 import time
 start = time.time()
-from ...CL.CL_Sets import Set
+from ....CL.CL_Sets import Set
 import math
+
 
 def IsInCube(P, C):
     if not(C[0][0] <= P[0] <= C[0][0] + C[1][0]):
@@ -11,6 +12,8 @@ def IsInCube(P, C):
     if not(C[0][2] <= P[2] <= C[0][2] + C[1][2]):
         return False
     return True
+
+
 def CubeVertices(C):
     ans = []
     ans.append([C[0][0], C[0][1], C[0][2]])
@@ -22,6 +25,8 @@ def CubeVertices(C):
     ans.append([C[0][0], C[0][1] + C[1][1], C[0][2] + C[1][2]])
     ans.append([C[0][0] + C[1][0], C[0][1] + C[1][1], C[0][2] + C[1][2]])
     return ans
+
+
 def AreConnected(C1, C2):
     for P in CubeVertices(C1):
         if IsInCube(P, C2):
@@ -30,6 +35,8 @@ def AreConnected(C1, C2):
         if IsInCube(P, C1):
             return True
     return False
+
+
 def DeletedCopies(lst):
     if lst == []:
         return []
@@ -40,6 +47,8 @@ def DeletedCopies(lst):
         if not ans[-1] == lst[i]:
             ans.append(lst[i])
     return ans
+
+
 def IntersectionCubes(C1, C2):
     x0 = max(C1[0][0], C2[0][0])
     y0 = max(C1[0][1], C2[0][1])
@@ -47,11 +56,15 @@ def IntersectionCubes(C1, C2):
     x1 = min(C1[0][0]+C1[1][0], C2[0][0]+C2[1][0])
     y1 = min(C1[0][1]+C1[1][1], C2[0][1]+C2[1][1])
     z1 = min(C1[0][2]+C1[1][2], C2[0][2]+C2[1][2])
+    if x1 < x0 or y1 < y0 or z1 < z0:
+        print(C1)
+        print(C2)
+        print(AreConnected(C1, C2))
     return [[x0, y0, z0], [x1-x0, y1-y0, z1-z0]]
+
+
 def VolumeCube(C):
     return C[1][0]*C[1][1]*C[1][2]
-
-
 
 
 def TotalCubesVolume(Cubes, SideLength, BiggestCube, numCubes):
@@ -62,7 +75,7 @@ def TotalCubesVolume(Cubes, SideLength, BiggestCube, numCubes):
     sply = [Set(elements = []) for _ in range(splits)]
     splz = [Set(elements = []) for _ in range(splits)]
 
-    C = [0] + Cubes
+    C = [0] + Cubes #Indexing issues
     #Create the splits
     for s in range(splits):
         lb = splitbounds[s]
@@ -120,12 +133,14 @@ def TotalCubesVolume(Cubes, SideLength, BiggestCube, numCubes):
         #In this loop we will be looking for k-cliques
         currcliques = []
         print(k,"-cliques to analyse = ", len(prevcliques))
+        if k == 4:
+            print(prevcliques)
         for p in prevcliques:
-            #Contribute to the sum
+            v = VolumeCube(p[1]) 
+            if v < 0:
+                for i in p[0]:
+                    print(C[i])
             ans += sign * VolumeCube(p[1]) 
-            #print(VolumeCube(p[1]) )
-            #Can we enlarge this clique? We will enlarge exclusively with a cube intersection that is smaller
-            #This ensures that we are counting each clique only once
             m = min(p[0])
             for v in p[2].bag:
                 if v < m:
@@ -144,18 +159,24 @@ Mod = 10_000
 mod = 399
 
 LIM = 50_000
-LIM = 1579
+#LIM = 1579
 #LIM = 100
 
 S = [0] + [(100003 - 200003*k+300007*k*k*k)%MOD for k in range(1, 55+1)]
 for i in range(56, LIM*6+1):
     S.append((S[i-24]+S[i-55])%MOD)
 
-C= [[[x, x, x], [2, 2, 2]] for x in range(10)]
-print(TotalCubesVolume(C, Mod, mod, 10))
+#C= [[[x, x, x], [2, 2, 2]] for x in range(10)]
+#print(TotalCubesVolume(C, Mod, mod, 10))
 
 
 C = [[[S[6*i-5]%Mod, S[6*i-4]%Mod, S[6*i-3]%Mod], [1 + S[6*i-2]%mod, 1 + S[6*i-1]%mod, 1 + S[6*i]%mod]] for i in range(1, LIM+1)]
 print("Answer = ", TotalCubesVolume(C, Mod, mod, LIM))
+
+print(C[42693])
+print(C[49888])
+print(C[38296])
+print(C[23770])
+
 end = time.time()
 print("Time elapsed ", end - start, " seconds")
