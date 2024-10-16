c62c20e0681c89ffaca3fe61eb7e5bb6cd614a61
raulpenaguiao <92503059+raulpenaguiao@users.noreply.github.com>
2024-10-16 19:25:55 +0200

    Update Euler430.py

--- problems_code/Euler04__/Euler430/Euler430.py
+++ problems_code/Euler04__/Euler430/Euler430.py
@@ -9,7 +9,8 @@ def E(N, M):
         int_sum += ((N*N - 4 * i * (N-i+1) + 2)**M)/((N*N)**M)
     return (N+int_sum)/2
 
-ans = E(10**4, 4000)
+ans = E(10**3, 4000)
+#ans = E(10**10, 4000)
 print("Answer = ", ans)
 end = time.time()
 print("Time elapsed ", end - start, " seconds")
